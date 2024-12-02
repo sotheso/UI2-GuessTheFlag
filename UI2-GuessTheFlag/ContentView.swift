aed34d3e781662367487fc4f911ba3775c7d8d8d
Sothesom <151612168+sotheso@users.noreply.github.com>
2024-12-02 16:07:01 +0330

    Part 1 project Guess the flag: [VStack], [ZStack], [HStack],[Color],[LinearGradient], [RadialGradient], [AngularGradient], [Buttons] ,[alert]

--- UI2-GuessTheFlag/ContentView.swift
+++ UI2-GuessTheFlag/ContentView.swift
@@ -8,17 +8,76 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria","Poland","Spain", "UK", "Ukraine"] .shuffled()
+    @State var correntAnswer = Int.random(in: 0...2)
+    
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            RadialGradient(stops: [
+                .init(color: .blue , location: 0.3),
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45 ) , location: 0.3)
+            ], center: .top, startRadius: 200, endRadius: 700)
+            .ignoresSafeArea()
+            
+            VStack(spacing:30){
+                Spacer()
+
+                Text("Guess the flag")
+                    .font(.largeTitle.bold())
+                    .foregroundStyle(Color(red: 0.1, green: 0.2, blue: 0.45 ))
+                VStack{
+                    Text("Tap the flag of")
+                        .foregroundStyle(.white)
+                        .font(.subheadline.weight(.heavy ))
+                    Text(countries[correntAnswer])
+                        .foregroundStyle(.white)
+                        .font(.largeTitle.weight(.semibold))
+                }
+                ForEach(0..<3) {number in
+                    Button{
+                        flagTapped(number)
+                    } label: {
+                        Image(countries[number])
+                            .clipShape(.buttonBorder)
+                            .shadow(radius: 15)
+                    }
+                }
+                Spacer()
+                Spacer()
+                Text(":امتیاز")
+                    .foregroundStyle(.white)
+                    .font(.title2.bold() )
+                
+                Spacer()
+
+            }
+            .padding()
         }
-        .padding()
+        .alert(scoreTitle ,isPresented: $showingScore){
+            Button("سوال بعدی", action: AskQ)
+        } message: {
+            Text("امتیاز شما ؟؟" )
+        }
+    }
+    
+    func flagTapped(_ nubmer: Int) {
+        if correntAnswer == nubmer {
+            scoreTitle = "درسته"
+        } else {
+            scoreTitle = "غلط"
+        }
+        showingScore = true
+    }
+    
+    func AskQ() {
+        countries.shuffle()
+        correntAnswer = Int.random(in: 0...2)
     }
 }
-
 #Preview {
     ContentView()
 }
