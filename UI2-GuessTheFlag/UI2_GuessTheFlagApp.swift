485989a1e6f2dddf2dbc92a41a012bd62a3e2e5f
Sothesom <151612168+sotheso@users.noreply.github.com>
2024-12-02 12:36:04 +0330

    Initial Commit

---
+++ UI2-GuessTheFlag/UI2_GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  UI2_GuessTheFlagApp.swift
+//  UI2-GuessTheFlag
+//
+//  Created by Sothesom on 12/09/1403.
+//
+
+import SwiftUI
+
+@main
+struct UI2_GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
